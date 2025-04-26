1bf79bf48a719e623f94c70e99b92ad7258d92ba
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2025-04-26 04:32:01 +0300

    rfct!: More ergonomic API (`Serialize` impl only)
    - Replace awkward `Vcard {[[VElement]]}` with a struct and custom `impl Serialize` for it
      (Deserialize is not fully supported yet, only some types have the `derive`)
    - Convert `mod tests` into doctests to make that code more useful.
    - Add documentation
    - Replace `Vcard::add_*`, `Property::new_*` constructors are used. `Vcard` only has a `push` method now.
    - Use more `impl Into<T>` as parameters to constructors to allow the user to suppy more types
      which are efficiently converted into the appropriate types.
    - Remove the `{cc: AT}` parameter from the address property. This parameter is not required by
      RFC 6350 (vCard) nor RFC 7095 (jCard), so it was removed. If you rely on this parameter, please add it at
      the call site.
    
    BREAKING CHANGE: Major changes to the crate's API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,179 +1,289 @@
-use crate::models::{Location, Telephone, VCard, VElement};
+use crate::models::{Address, Telephone};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 pub mod models;
+mod ser;
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct VCardArray {
-    elements: Vec<Vec<VElement>>,
+/// A jCard serde type
+#[derive(Debug, Clone, PartialEq)]
+pub struct Vcard {
+    /// A parsed out jCard version property.
+    pub version: String,
+    /// jCard properties
+    ///
+    /// # Notes
+    ///
+    /// - Do not include the `version` type property in this array. Instead, set the [`Vcard::version`] property.
+    ///   `["version,{},"text",version]` will be inserted by the [`Serialize`] implementation.
+    pub properties: Vec<Property>,
 }
 
-impl VCardArray {
-    pub fn new() -> Self {
-        VCardArray {
-            elements: vec![vec![
-                VElement::Element("version".to_string()),
-                VElement::Dictionary(HashMap::new()),
-                VElement::Element("text".to_string()),
-                VElement::Element("4.0".to_string()),
-            ]],
-        }
-    }
-
-    fn add_vcard(
-        &mut self,
-        category: String,
-        properties: HashMap<String, String>,
-        types: String,
-        value: VElement,
-    ) {
-        self.elements.push(vec![
-            VElement::Element(category),
-            VElement::Dictionary(properties),
-            VElement::Element(types),
-            value,
-        ]);
-    }
-
-    pub fn add_fn(&mut self, name: String, surname: String) {
-        self.add_vcard(
-            "fn".to_string(),
-            HashMap::new(),
-            "text".to_string(),
-            VElement::Element(format!("{} {}", name, surname)),
-        )
-    }
-
-    pub fn add_org(&mut self, name: Option<String>, unit: Option<String>) {
-        self.add_vcard(
-            "org".to_string(),
-            HashMap::new(),
-            "text".to_string(),
-            VElement::Element(format!(
-                "{} {}",
-                name.unwrap_or_else(|| "".to_string()),
-                unit.unwrap_or_else(|| "".to_string())
-            )),
-        )
-    }
-
-    pub fn add_address(&mut self, location: Location) {
-        let mut properties: HashMap<String, String> = HashMap::new();
-
-        properties.insert("cc".to_string(), "AT".to_string());
-
-        // Array pn given order
-        // the post office box;
-        // the extended address (e.g., apartment or suite number);
-        // the street address;
-        // the locality (e.g., city);
-        // the region (e.g., state or province);
-        // the postal code;
-        // the country name (full name);
-        self.add_vcard(
-            "adr".to_string(),
-            properties,
-            "text".to_string(),
-            VElement::Array(vec![
-                location.post_office_box.unwrap_or_else(|| "".to_string()),
-                location.extended_address.unwrap_or_else(|| "".to_string()),
-                location.street_address.unwrap_or_else(|| "".to_string()),
-                location.locality.unwrap_or_else(|| "".to_string()),
-                location.region.unwrap_or_else(|| "".to_string()),
-                location.postal_code.unwrap_or_else(|| "".to_string()),
-                location.country.unwrap_or_else(|| "".to_string()),
-            ]),
-        )
-    }
-
-    pub fn add_tel(&mut self, types: Telephone, number: String) {
-        let mut properties: HashMap<String, String> = HashMap::new();
-
-        match types {
-            Telephone::Voice => {
-                properties.insert("type".to_string(), "voice".to_string());
-            }
-            Telephone::Fax => {
-                properties.insert("type".to_string(), "fax".to_string());
-            }
-        }
-
-        self.add_vcard(
-            "tel".to_string(),
-            properties,
-            "uri".to_string(),
-            VElement::Element(format!("tel:{}", number)),
-        )
-    }
-
-    pub fn add_email(&mut self, email: String) {
-        self.add_vcard(
-            "email".to_string(),
-            HashMap::new(),
-            "text".to_string(),
-            VElement::Element(email),
-        );
-    }
-
-    pub fn to_json(&self, pretty: bool) -> String {
-        let array: Vec<VCard> = vec![
-            VCard::Element("vcard".to_string()),
-            VCard::ElementArray(self.elements.clone()),
-        ];
-
-        match pretty {
-            true => serde_json::to_string_pretty(&array).unwrap(),
-            false => serde_json::to_string(&array).unwrap(),
+impl Default for Vcard {
+    fn default() -> Self {
+        Self {
+            version: "4.0".into(),
+            properties: Vec::default(),
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::{Location, VCardArray};
+/// An entry in the jCard.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Property {
+    /// Aka the type. E.g. `"fn"`.
+    pub name: String,
 
-    #[test]
-    fn test_fn() {
-        let mut vcard = VCardArray::new();
+    /// The list of parameters such as the laguage or the preference value.
+    pub parameters: HashMap<String, String>,
 
-        vcard.add_fn("John".to_string(), "Doe".to_string());
+    /// The value type. E.g. `"text"`
+    pub value_type: String,
 
-        let result =
-            "[\"vcard\",[[\"version\",{},\"text\",\"4.0\"],[\"fn\",{},\"text\",\"John Doe\"]]]"
-                .to_string();
-        assert_eq!(vcard.to_json(false), result);
+    /// Either a single or multiple values of the jCard property.
+    ///
+    /// When the array has multiple elements, they are appended at the level of the property array in jCard format:
+    ///
+    /// ```json
+    /// ["categories", {}, "text", "rust", "serde"]
+    /// ```
+    ///
+    /// Where rust and serde are [`PropertyValue::String`]
+    ///
+    /// For a structured property, use [`PropertyValue::Structured`]. See [`Property::new_adr`] for an example of a
+    /// structured property.
+    pub values: Vec<PropertyValue>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum PropertyValue {
+    String(String),
+    Structured(Vec<String>),
+}
+
+impl Vcard {
+    /// Appends a property to the vCard.
+    ///
+    /// Check the examples for [`Property`]'s constructors for examples of how to easily append different vCard
+    /// properties.
+    pub fn push(&mut self, property: impl Into<Property>) {
+        self.properties.push(property.into());
+    }
+}
+
+impl Property {
+    /// Creates a new property, where [`Property::values`] is a `vec![value]`.
+    pub fn new(
+        name: impl ToString,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+        value_type: impl ToString,
+        value: impl Into<PropertyValue>,
+    ) -> Self {
+        Self::new_multivalued(name, parameters, value_type, vec![value.into()])
     }
 
-    #[test]
-    fn test_adr() {
-        let mut vcard = VCardArray::new();
-
-        vcard.add_address(Location {
-            post_office_box: None,
-            extended_address: None,
-            street_address: Some("Jakob-Haringer-Strasse 8/V".to_string()),
-            locality: Some("Salzburg".to_string()),
-            region: Some("Salzburg".to_string()),
-            postal_code: Some(5020.to_string()),
-            country: None,
-        });
-
-        let result =
-            "[\"vcard\",[[\"version\",{},\"text\",\"4.0\"],[\"adr\",{\"cc\":\"AT\"},\"text\",[\"\",\"\",\"Jakob-Haringer-Strasse 8/V\",\"Salzburg\",\"Salzburg\",\"5020\",\"\"]]]]"
-                .to_string();
-        assert_eq!(vcard.to_json(false), result);
+    pub fn new_multivalued(
+        name: impl ToString,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+        value_type: impl ToString,
+        values: Vec<PropertyValue>,
+    ) -> Self {
+        Self {
+            name: name.to_string(),
+            parameters: parameters.into().unwrap_or_default(),
+            value_type: value_type.to_string(),
+            values,
+        }
     }
 
-    #[test]
-    fn test_email() {
-        let mut vcard = VCardArray::new();
+    /// # Example
+    ///
+    /// ```rust
+    /// # use vicardi::models::*;
+    /// # use vicardi::*;
+    /// # use serde_json::json;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut vcard = Vcard::default();
+    /// vcard.push(Property::new_fn("John Doe", None));
+    ///
+    /// let json = json!([
+    ///     "vcard",
+    ///     [
+    ///         ["version", {}, "text", "4.0"],
+    ///         ["fn", {}, "text", "John Doe"]
+    ///     ]
+    /// ]);
+    ///
+    /// assert_eq!(serde_json::to_value(vcard)?, json);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new_fn(
+        formatted: impl ToString,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+    ) -> Self {
+        Self::new("fn", parameters, "text", formatted)
+    }
 
-        vcard.add_email("sample@mail.com".to_string());
+    /// # Example
+    /// ```rust
+    /// # use vicardi::models::*;
+    /// # use vicardi::*;
+    /// # use serde_json::json;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut vcard = Vcard::default();
+    ///
+    /// let address = Address {
+    ///     street_address: "Jakob-Haringer-Strasse 8/V".to_string(),
+    ///     locality: "Salzburg".to_string(),
+    ///     region: "Salzburg".to_string(),
+    ///     postal_code: 5020.to_string(),
+    ///
+    ///     ..Default::default()
+    /// };
+    ///
+    /// vcard.push(Property::new_adr(
+    ///     address.clone(),
+    ///     Some([("pref".to_string(), "1".to_string())].into_iter().collect())
+    /// ));
+    /// vcard.push(address);
+    ///
+    /// let json = json!([
+    ///     "vcard",
+    ///     [
+    ///         ["version", {}, "text", "4.0"],
+    ///
+    ///         ["adr", {"pref": "1"}, "text", [
+    ///             "",
+    ///             "",
+    ///             "Jakob-Haringer-Strasse 8/V",
+    ///             "Salzburg",
+    ///             "Salzburg",
+    ///             "5020",
+    ///             ""
+    ///         ]],
+    ///         ["adr", {}, "text", [
+    ///             "",
+    ///             "",
+    ///             "Jakob-Haringer-Strasse 8/V",
+    ///             "Salzburg",
+    ///             "Salzburg",
+    ///             "5020",
+    ///             ""
+    ///         ]]
+    ///     ]
+    /// ]);
+    ///
+    /// assert_eq!(serde_json::to_value(vcard)?, json);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new_adr(
+        address: Address,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+    ) -> Self {
+        Self::new("adr", parameters, "text", address)
+    }
 
-        let result =
-            "[\"vcard\",[[\"version\",{},\"text\",\"4.0\"],[\"email\",{},\"text\",\"sample@mail.com\"]]]"
-                .to_string();
-        assert_eq!(vcard.to_json(false), result);
+    ///
+    /// /// # Example
+    /// /**
+    /// ```rust
+    /// # use vicardi::models::*;
+    /// # use vicardi::*;
+    /// # use serde_json::json;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut vcard = Vcard::default();
+    /// vcard.push(Property::new_org("Vicardi", None));
+    /// vcard.push(Property::new_org(PropertyValue::Structured(vec!["Vicardi".into(), "Rust development".into()]), None));
+    ///
+    /// let json = json!([
+    ///     "vcard",
+    ///     [
+    ///         ["version", {}, "text", "4.0"],
+    ///         ["org", {}, "text", "Vicardi"],
+    ///         ["org", {}, "text", ["Vicardi", "Rust development"]]
+    ///     ]
+    /// ]);
+    ///
+    /// assert_eq!(serde_json::to_value(vcard)?, json);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new_org(
+        org: impl Into<PropertyValue>,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+    ) -> Self {
+        Self::new("org", parameters, "text", org)
+    }
+
+    /// # Example
+    /// ```rust
+    /// # use vicardi::models::*;
+    /// # use vicardi::*;
+    /// # use serde_json::json;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut vcard = Vcard::default();
+    /// vcard.push(Property::new_tel(Telephone::Voice, "+1-555-555-5555", None));
+    ///
+    /// let json = json!([
+    ///     "vcard",
+    ///     [
+    ///         ["version", {}, "text", "4.0"],
+    ///         ["tel", {"type": "voice"}, "uri", "tel:+1-555-555-5555"]
+    ///     ]
+    /// ]);
+    ///
+    /// assert_eq!(serde_json::to_value(vcard)?, json);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new_tel(
+        phone_type: impl Into<Telephone>,
+        number: impl AsRef<str>,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+    ) -> Self {
+        let mut parameters = parameters.into().unwrap_or_default();
+        parameters.insert("type".into(), phone_type.into().to_string());
+
+        Self::new("tel", parameters, "uri", format!("tel:{}", number.as_ref()))
+    }
+
+    /// # Example
+    /// ```rust
+    /// # use vicardi::models::*;
+    /// # use vicardi::*;
+    /// # use serde_json::json;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut vcard = Vcard::default();
+    /// vcard.push(Property::new_email("vicardi@example.com", None));
+    ///
+    /// let json = json!([
+    ///     "vcard",
+    ///     [
+    ///         ["version", {}, "text", "4.0"],
+    ///         ["email", {}, "text", "vicardi@example.com"]
+    ///     ]
+    /// ]);
+    ///
+    /// assert_eq!(serde_json::to_value(vcard)?, json);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new_email(
+        email: impl ToString,
+        parameters: impl Into<Option<HashMap<String, String>>>,
+    ) -> Self {
+        Self::new("email", parameters, "text", email.to_string())
+    }
+}
+
+impl<T> From<T> for PropertyValue
+where
+    T: ToString,
+{
+    fn from(value: T) -> Self {
+        PropertyValue::String(value.to_string())
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,39 +1,75 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{convert::Infallible, fmt::Display, str::FromStr};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", untagged)]
-pub enum VElement {
-    Element(String),
-    Dictionary(HashMap<String, String>),
-    Array(Vec<String>),
+use serde_with::{DeserializeFromStr, SerializeDisplay};
+
+use crate::{Property, PropertyValue};
+
+#[derive(Debug, Clone, Default)]
+pub struct Address {
+    pub post_office_box: String,
+    pub extended_address: String,
+    pub street_address: String,
+    pub locality: String,
+    pub region: String,
+    pub postal_code: String,
+    pub country: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", untagged)]
-pub enum VCard {
-    Element(String),
-    ElementArray(Vec<Vec<VElement>>),
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum VCardField {
-    Version,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Location {
-    pub post_office_box: Option<String>,
-    pub extended_address: Option<String>,
-    pub street_address: Option<String>,
-    pub locality: Option<String>,
-    pub region: Option<String>,
-    pub postal_code: Option<String>,
-    pub country: Option<String>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, SerializeDisplay, DeserializeFromStr)]
 pub enum Telephone {
     Fax,
     Voice,
+    Other(String),
+}
+
+impl From<Address> for PropertyValue {
+    fn from(address: Address) -> Self {
+        PropertyValue::Structured(
+            [
+                address.post_office_box,
+                address.extended_address,
+                address.street_address,
+                address.locality,
+                address.region,
+                address.postal_code,
+                address.country,
+            ]
+            .into_iter()
+            .collect(),
+        )
+    }
+}
+
+impl From<Address> for Property {
+    fn from(address: Address) -> Self {
+        Self::new("adr", None, "text", address)
+    }
+}
+
+impl Display for Telephone {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
+}
+
+impl AsRef<str> for Telephone {
+    fn as_ref(&self) -> &str {
+        match self {
+            Self::Fax => "fax",
+            Self::Voice => "voice",
+            Self::Other(other) => other.as_ref(),
+        }
+    }
+}
+
+impl FromStr for Telephone {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "fax" => Self::Fax,
+            "voice" => Self::Voice,
+            other => Self::Other(other.to_string()),
+        })
+    }
 }
